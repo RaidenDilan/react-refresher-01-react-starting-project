2335f8d3120cbd036e75c01357d31d42b45fb384
RaidenDilan <raiden18@me.com>
2020-04-24 01:51:42 +0100

    Rendering List of Data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import GoalList from './components/GoalList';
+import './App.css';
+
 const App = () => {
-  return <h1>A React App!</h1>;
+  const courseGoals = [
+    { id: 'cg1', text: 'Finish the Course' },
+    { id: 'cg2', text: 'Learn all about the course main topic.' },
+    { id: 'cg3', text: 'Help other students in the course Q&A.' }
+  ];
+
+  return (
+    <div className='course-goals'>
+      <h2>Course Goals</h2>
+      <GoalList goals={ courseGoals } />
+    </div>
+  );
 };
+
+// class App extends React.Component {
+//   render() {
+//     return <h1 title='This Works!'>Hi, <span>this</span> is ReactJS!</h1>;
+//   }
+// }
 
 export default App;
